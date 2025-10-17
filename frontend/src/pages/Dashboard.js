c1ad78deec668cd7892483b92daa8f2a698a8c59
Willy Saronamihardja <wsaronam@gmail.com>
2025-10-17 03:14:01 -0700

    updates
    - added code for getting some data from spotify
    - still need to test the code

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 
@@ -6,31 +6,53 @@ import axios from "axios";
 
 function Dashboard() {
 
+    const [user, setUser] = useState(null);
+    const [tracks, setTracks] = useState([]);
+
     useEffect(() => {
 
-        const getUser = async () => {
-            try {
-                const token = localStorage.getItem("spotify_token");
-                const res = await fetch("https://api.spotify.com/v1/me", {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
+        const getData = async () => {
+            const token = localStorage.getItem("spotify_token");
 
-                const data = await res.json();
-                console.log(data);
-            }
-            catch (err) {
-                console.log(err);
-            }
+            // get user profile
+            const userRes = await fetch("https://api.spotify.com/v1/me", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            const userData = await userRes.json();
+            setUser(userData);
+
+
+            // get liked songs
+            const tracksRes = await fetch("https://api.spotify.com/v1/me/tracks?limit=50", {
+                headers: { Authorization: `Bearer ${token}`, },
+            });
+            const tracksData = await tracksRes.json();
+            return tracksData.items.map(item => item.track);
         }
 
-        getUser();
+        getData();
     }, []);
+
+    
+
 
     return (
         <div>
             <h1>Dashboard Page</h1>
+            <h1>Welcome, {user.display_name}</h1>
+            <img src={user.images?.[0]?.url} alt="profile_picture"/>
+
+            <h2>Liked Songs</h2>
+            <ul>
+                {tracks.map((track) => (
+                    <li key={track.id}>
+                        {track.name} - {track.artists.name}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
